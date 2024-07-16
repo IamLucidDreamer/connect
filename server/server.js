cda8af9f3f7dc034425c79b344514c4224313ae3
A635WEB <aditya.ats23@gmail.com>
2024-07-17 01:13:16 +0530

    Cors

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const express = require('express');
-// const cors = require('cors');
+const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const logger = require('./utils/logger');
@@ -18,13 +18,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-    origin: 'http://localhost:5173', // Allow only this origin
+    origin: ['http://localhost:3000', "https://new.d1ivogcauayjj5.amplifyapp.com/" ], // Allow only this origin
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     optionsSuccessStatus: 204,
   };
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/api', routes);
 
